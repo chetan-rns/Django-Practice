528b2af8c5ea71d43c301a7b8938fa873f14fe94
chetanrns <chetanrns1997@gmail.com>
2018-10-23 18:42:02 +0530

    populated database

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -4,8 +4,9 @@ from rango.models import Category,Page
 # Create your views here.
 
 def index(request):
-	context_dict={'boldmessage':'Crunchy,creamy,cookie,candie'}
-	return render(request, 'rango/index.html',context=context_dict)
+	category_list=Category.objects.order_by('-likes')[:5]
+	context_dict={'categories':category_list}
+	return render(request, 'rango/index.html',context_dict)
 
 def about(request):
 	develop_context={'developer':'chetan'}
